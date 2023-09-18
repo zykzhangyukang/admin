f81491ecb1a32cf1bf8a5313dafa722e045a1b16
zhangyukang <3053161401@qq.com>
2023-09-18 18:17:03 +0800

    ffmpeg优化

--- bizedu-web/src/test/java/FfmepgTest.java
+++ bizedu-web/src/test/java/FfmepgTest.java
@@ -5,10 +5,12 @@ import net.bramp.ffmpeg.FFmpegUtils;
 import net.bramp.ffmpeg.FFprobe;
 import net.bramp.ffmpeg.builder.FFmpegBuilder;
 import net.bramp.ffmpeg.job.FFmpegJob;
+import net.bramp.ffmpeg.probe.FFmpegFormat;
 import net.bramp.ffmpeg.probe.FFmpegProbeResult;
 import net.bramp.ffmpeg.progress.Progress;
 import net.bramp.ffmpeg.progress.ProgressListener;
 import org.springframework.util.Assert;
+import org.springframework.util.SocketUtils;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -30,30 +32,44 @@ public class FfmepgTest {
         String inputVideoPath = "C:\\Users\\Administrator\\Desktop\\test\\input.mp4";
         // 输出M3U8文件路径
         String outputM3U8Path = "C:\\Users\\Administrator\\Desktop\\test\\output\\output.m3u8";
+        // 视频封面
+        String outputImagePath = "C:\\Users\\Administrator\\Desktop\\test\\output\\output_image.jpg"; // 输出图像文件路径
 
-        // ffmpeg -i C:\Users\Administrator\Desktop\test\input.mp4 -codec: copy  -start_number 0 -hls_time 10 -hls_list_size 0 -f hls C:\Users\Administrator\Desktop\test\output\output.m3u8
+
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+        FFmpegProbeResult in = ffprobe.probe(inputVideoPath);
+        FFmpegFormat format = in.getFormat();
+
+        //【mp4->m3u8】 ffmpeg -i C:\Users\Administrator\Desktop\test\input.mp4 -codec: copy  -start_number 0 -hls_time 10 -hls_list_size 0 -f hls C:\Users\Administrator\Desktop\test\output\output.m3u8
         FFmpegBuilder builder = new FFmpegBuilder()
                 .setInput(inputVideoPath)
                 .addOutput(outputM3U8Path)
                 .setFormat("hls")
-//                .addExtraArgs("-codec:", "copy")
+                .addExtraArgs("-codec:", "copy")
                 .addExtraArgs("-start_number", "0")
                 .addExtraArgs("-hls_time", "10")
                 .addExtraArgs("-hls_list_size", "0")
                 .done();
 
-        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
-        FFmpegProbeResult in = ffprobe.probe(inputVideoPath);
+        // 【截取封面】指定要截取的时间点（这里截取视频中间的一帧）
+        long durationMillis = (long) (format.duration * 1000);
+        long captureTimeMillis = durationMillis / 2;
+        FFmpegBuilder builder2 = new FFmpegBuilder()
+                .setInput(inputVideoPath)
+                .addOutput(outputImagePath)
+                .addExtraArgs("-ss", String.valueOf(captureTimeMillis / 1000)) // 截取时间点
+                .addExtraArgs("-vframes", "1") // 截取一帧图像
+                .done();
 
         FFmpegJob fFmpegJob = null;
         try {
             fFmpegJob = executor.createJob(builder, new ProgressListener() {
-                final double duration_ns = in.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
+                final double duration_ns = format.duration * TimeUnit.SECONDS.toNanos(1);
                 @Override
                 public void progress(Progress progress) {
                     double percentage = progress.out_time_ns / duration_ns;
                     // Print out interesting information about the progress
-                    System.out.println(Thread.currentThread().getName() + "----------->" + String.format(
+                    System.out.println(Thread.currentThread().getName() + "-----"+format.duration+"s------>" + String.format(
                             "[%.0f%%] status:%s frame:%d time:%s ms fps:%.0f speed:%.2fx",
                             percentage * 100,
                             progress.status,
