451f61d2cfefa830a9c6bd25852cd8c41542a9bc
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2023-11-05 12:04:15 +0800

    添加消息回调机制+1

--- bizedu-service/src/main/java/com/coderman/bizedu/service/sync/impl/CallbackServiceImpl.java
+++ bizedu-service/src/main/java/com/coderman/bizedu/service/sync/impl/CallbackServiceImpl.java
@@ -1,24 +1,37 @@
 package com.coderman.bizedu.service.sync.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.coderman.api.constant.RedisDbConstant;
 import com.coderman.api.util.ResultUtil;
 import com.coderman.api.vo.ResultVO;
 import com.coderman.bizedu.service.sync.CallbackService;
 import com.coderman.callback.SyncCallback;
 import com.coderman.callback.SyncMsg;
+import com.coderman.redis.service.RedisService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 @Slf4j
 public class CallbackServiceImpl implements CallbackService {
 
+    @Resource
+    private RedisService redisService;
 
     @Override
     @SyncCallback(value = "update_auth_bizedu_user")
     public ResultVO<Void> updateUserCallback(SyncMsg syncMsg) {
 
+        long count = this.redisService.incr(syncMsg.getMsgId(), RedisDbConstant.REDIS_DB_DEFAULT);
+        if (count >= 4) {
 
+            this.redisService.del(syncMsg.getMsgId(), RedisDbConstant.REDIS_DB_DEFAULT);
+            log.error("回调成功，msgId:{}", syncMsg.getMsgId());
+
+            return ResultUtil.getSuccess("回调成功。。。。。。");
+        }
 
         log.info("updateUserCallback回调，syncMsg:{}", JSON.toJSONString(syncMsg));
 
