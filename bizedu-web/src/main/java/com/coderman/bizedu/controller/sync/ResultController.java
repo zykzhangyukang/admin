611ae447cb8d56577c2991ce171e82e692c91260
zhangyukang <3053161401@qq.com>
2023-11-08 14:15:51 +0800

    接口新增

---
+++ bizedu-web/src/main/java/com/coderman/bizedu/controller/sync/ResultController.java
@@ -0,0 +1,87 @@
+package com.coderman.bizedu.controller.sync;
+
+import com.coderman.api.vo.PageVO;
+import com.coderman.bizedu.dto.sync.ResultPageDTO;
+import com.coderman.bizedu.dto.sync.ResultValidDataDTO;
+import com.coderman.bizedu.model.sync.ResultModel;
+import com.coderman.bizedu.sync.service.result.ResultService;
+import com.coderman.bizedu.utils.AuthUtil;
+import com.coderman.bizedu.vo.sync.CompareVO;
+import com.coderman.swagger.annotation.ApiReturnParam;
+import com.coderman.swagger.annotation.ApiReturnParams;
+import com.coderman.swagger.constant.SwaggerConstant;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Administrator
+ */
+@Api(value = "同步记录", tags = "同步记录")
+@RestController
+@RequestMapping(value = "/sync/result")
+public class ResultController {
+
+    @Resource
+    private ResultService resultService;
+
+    @ApiOperation(httpMethod = SwaggerConstant.METHOD_POST, value = "同步记录查询")
+    @PostMapping(value = "/page")
+    @ApiReturnParams({
+            @ApiReturnParam(name = "ResultVO", value = {"code", "msg", "result"}),
+            @ApiReturnParam(name = "PageVO", value = {"pageRow", "totalRow", "currPage", "totalPage", "dataList"}),
+            @ApiReturnParam(name = "ResultModel", value = {"syncToEs", "msgSrc", "syncTime", "errorMsg", "uuid", "planCode", "msgId", "planName", "mqId", "planUuid", "status", "repeatCount", "msgCreateTime",
+                    "msgContent", "destProject", "srcProject", "syncContent", "remark"})
+    })
+    public com.coderman.api.vo.ResultVO<PageVO<List<ResultModel>>> page(@RequestBody ResultPageDTO resultPageDTO) throws Exception {
+        return this.resultService.page(resultPageDTO);
+    }
+
+    @ApiOperation(httpMethod = SwaggerConstant.METHOD_PUT, value = "重新同步")
+    @PutMapping(value = "/repeat/sync")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "uuid", paramType = SwaggerConstant.PARAM_QUERY, dataType = SwaggerConstant.DATA_STRING, value = "uuid", required = true),
+    })
+    @ApiReturnParams({
+            @ApiReturnParam(name = "ResultVO", value = {"code", "msg", "result"}),
+    })
+    public com.coderman.api.vo.ResultVO<Void> repeatSync(String uuid) {
+
+        return this.resultService.repeatSync(uuid);
+    }
+
+    @ApiOperation(httpMethod = SwaggerConstant.METHOD_PUT, value = "手动标记成功")
+    @PutMapping(value = "/sign/success")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "uuid", paramType = SwaggerConstant.PARAM_QUERY, dataType = SwaggerConstant.DATA_STRING, value = "uuid", required = true),
+    })
+    @ApiReturnParams({
+            @ApiReturnParam(name = "ResultVO", value = {"code", "msg", "result"}),
+    })
+    public com.coderman.api.vo.ResultVO<Void> signSuccess(String uuid) throws IOException {
+
+        return this.resultService.signSuccess(uuid, AuthUtil.getCurrent().getRealName() + "手动标记成功");
+    }
+
+    @ApiOperation(httpMethod = SwaggerConstant.METHOD_POST, value = "校验同步结果")
+    @PostMapping(value = "/valid/data")
+    @ApiReturnParams({
+            @ApiReturnParam(name = "ResultVO", value = {"code", "msg", "result"}),
+            @ApiReturnParam(name = "CompareVO", value = {"srcUnique", "destColumnList", "srcResultList", "destResultList", "srcColumnList", "destUnique", "destTable", "srcTable"}),
+    })
+    public com.coderman.api.vo.ResultVO<List<CompareVO>> validResultData(@RequestBody ResultValidDataDTO resultValidDataDTO) throws Throwable {
+
+        String msgContent = resultValidDataDTO.getMsgContent();
+        Assert.notNull(msgContent, "msgContent is null");
+
+        return this.resultService.selectTableData(msgContent, true);
+    }
+
+}
