ab714e96f836446df27709dcee48605d13560c82
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2023-11-10 11:07:05 +0800

    ES配置加密

--- bizedu-web/src/test/java/DesUtil.java
+++ bizedu-web/src/test/java/DesUtil.java
@@ -5,7 +5,9 @@
 public class DesUtil {
 
     public static void main(String[] args) {
-        String encrypt = com.coderman.service.util.DesUtil.encrypt("123456","1123456");
+        String encrypt = com.coderman.service.util.DesUtil.encrypt("es.coderman.love:443","fdasfsf");
         System.out.println(encrypt);
+        String decrypt = com.coderman.service.util.DesUtil.decrypt("FFA413173ED6FAA15A70EF283CD06271987E71D6A0ED1F3C", "fdsafdsrf");
+        System.out.println(decrypt);
     }
 }
