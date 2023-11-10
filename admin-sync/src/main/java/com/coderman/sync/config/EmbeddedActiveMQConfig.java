bcd0a2898cf275a2b2bca74658f52e3f38d1c74f
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2023-11-10 16:12:01 +0800

    代码重构

--- admin-sync/src/main/java/com/coderman/sync/config/EmbeddedActiveMQConfig.java
+++ admin-sync/src/main/java/com/coderman/sync/config/EmbeddedActiveMQConfig.java
@@ -1,4 +1,4 @@
-package com.coderman.sync.server;
+package com.coderman.sync.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
