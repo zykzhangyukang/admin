ebda527c46a12896d820e81b4ffb66a1a1b91194
zhangyukang <3053161401@qq.com>
2024-08-13 12:25:55 +0800

    配置rocketmq支持+1

--- admin-service/src/main/java/com/coderman/admin/sync/config/EmbeddedActiveMQConfig.java
+++ admin-service/src/main/java/com/coderman/admin/sync/config/EmbeddedActiveMQConfig.java
@@ -5,14 +5,13 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.Profile;
 
 /**
  * @author 嵌入式activeMQ配置
  */
 @ImportResource("classpath:embedded-activemq.xml")
 @EnableConfigurationProperties({SyncProperties.class})
-@ConditionalOnProperty(prefix = "sync", name = "mq", havingValue = "rocketmq")
+@ConditionalOnProperty(prefix = "sync", name = "mq", havingValue = "activemq")
 @Configuration
 public class EmbeddedActiveMQConfig {
 }
